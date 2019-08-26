9ebaf58b65f73a809198333945cadc8823653617
idealab-kids <idealab-kids>
2019-08-26 09:31:02 -0500

    init

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1566829143,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player1-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/solid-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/gline1-sheet0.png",
+		"images/warpball-sheet0.png",
+		"images/warpballp1-sheet0.png",
+		"images/warpballp2-sheet0.png",
+		"images/stun1-sheet0.png",
+		"images/wrong-sheet0.png",
+		"images/right_selct-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
